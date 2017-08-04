53bc00e207944c6b1cdcc981d9a4329c6e5b16ac
LMoon <jjay033033@gmail.com>
2017-08-04 12:20:38 +0800

    123

--- src/main/java/top/lmoon/spring/util/SpringContextUtil.java
+++ src/main/java/top/lmoon/spring/util/SpringContextUtil.java
@@ -6,12 +6,14 @@ package top.lmoon.spring.util;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 /**
  * @author LMoon
  * @date 2017年8月4日
  * 
  */
+@Component
 public class SpringContextUtil implements ApplicationContextAware{
 	
 	private static ApplicationContext applicationContext;
@@ -22,7 +24,7 @@ public class SpringContextUtil implements ApplicationContextAware{
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		// TODO Auto-generated method stub
-		this.applicationContext = applicationContext;
+		SpringContextUtil.applicationContext = applicationContext;
 	}
 	
 	public static ApplicationContext getApplicationContext() {  
