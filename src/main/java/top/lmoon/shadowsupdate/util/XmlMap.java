a7902e7b5900d5dd5618d92bf3329a666c780c03
LMoon <jjay033033@gmail.com>
2017-08-04 10:11:46 +0800

    123

--- src/main/java/top/lmoon/shadowsupdate/util/XmlMap.java
+++ src/main/java/top/lmoon/shadowsupdate/util/XmlMap.java
@@ -1,7 +1,5 @@
 package top.lmoon.shadowsupdate.util;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Map;
 
@@ -9,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.util.HtmlUtils;
 
 import top.lmoon.heroku.dao.ConfsDAO;
-import top.lmoon.shadowsupdate.util.XmlUtil;
 
 public class XmlMap {
 	
@@ -29,7 +26,9 @@ public class XmlMap {
 //		map = XmlUtil.toMap(Thread.currentThread().getContextClassLoader().getResourceAsStream(path));
 //		String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><configs>	<sleepTime>300000</sleepTime>	<servers>		<server>			<id>ishadowsocks</id>			<url>http://isx.yt</url>			<begin>id=&quot;portfolio&quot;</begin>			<end>id=&quot;footer&quot;</end>			<type>0</type>			<serverIpBegin>IP Address:&lt;span[^&gt;]*&gt;</serverIpBegin>			<serverIpEnd>&lt;</serverIpEnd>			<serverPortBegin>Port：</serverPortBegin>			<serverPortEnd>&lt;</serverPortEnd>			<passwordBegin>Password:&lt;span[^&gt;]*&gt;</passwordBegin>			<passwordEnd>&lt;</passwordEnd>			<encryptionBegin>Method:</encryptionBegin>			<encryptionEnd>&lt;</encryptionEnd>		</server>		<server>			<id>shadowsocks8</id>			<url>http://my.shadowsocks8.org/</url>			<begin>&lt;section id=&quot;free&quot;</begin>			<end>&lt;section id=&quot;sslist&quot;</end>			<type>1</type>			<picUrlBegin>&lt;img src=&quot;</picUrlBegin>			<picUrlEnd>&quot;</picUrlEnd>			<severPicFlag>server</severPicFlag>		</server>		<server>			<id>freevpnss</id>			<url>https://get.freevpnss.me/</url>			<begin>免费SS帐号</begin>			<end>商家推荐</end>			<type>0</type>			<serverIpBegin>服务器地址：</serverIpBegin>			<serverIpEnd>&lt;</serverIpEnd>			<serverPortBegin>端口：</serverPortBegin>			<serverPortEnd>&lt;</serverPortEnd>			<passwordBegin>码：</passwordBegin>			<passwordEnd>&lt;</passwordEnd>			<encryptionBegin>加密方式：</encryptionBegin>			<encryptionEnd>&lt;</encryptionEnd>		</server>	</servers></configs>";
 //		map = XmlUtil.toMap(xml);
-		map = XmlUtil.toMap(HtmlUtils.htmlUnescape(confsDAO.selectConf()));
+		String a = confsDAO.selectConf();
+		String b = HtmlUtils.htmlUnescape(a);
+		map = XmlUtil.toMap(b);
 	}
 
 	public Map getConfigMap() throws FileNotFoundException {
