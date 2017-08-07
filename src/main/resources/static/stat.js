38f3935767f309e89d45c4b12ebb1f26cabb0e85
LMoon <jjay033033@gmail.com>
2017-08-07 12:01:20 +0800

    123

--- src/main/resources/static/stat.js
+++ src/main/resources/static/stat.js
@@ -1,10 +1,11 @@
 var cip = returnCitySN["cip"];
 var cname = returnCitySN["cname"];
+var capp = window.location.pathname;
 $.ajax({
 	type : "GET",// post
 	url : "https://vast-inlet-75928.herokuapp.com/vipAdd",
 	data : {
-		ip : cip + "," + cname + ",e1"
+		ip : cip + "," + cname + ","+capp
 	},
 	dataType : "JSONP",
 	success : function(data) {
