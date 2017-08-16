f49103436c66fd5162c390d38ba611f4ad9dd804
LMoon <jjay033033@gmail.com>
2017-08-16 14:45:22 +0800

    123

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$('.pagination').jqPagination({
 		link_string	: '/vipGet?pageNo={page_number}',
-		max_page	: $('#total.text'),
+		max_page	: $('#total').text,
 		paged		: function(page) {
 			$('.log').prepend('<li>Requested page ' + page + '</li>');
 		}
