9a0adb7fc8c73702e23573b0e3aab4f82e96ae13
LMoon <jjay033033@gmail.com>
2017-08-16 16:36:02 +0800

    123

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 		link_string	: '/vipGet?pageNo={page_number}',
 		max_page	: $('#total').val(),
 		paged		: function(page) {
+			alert(page);
 			$('.log').prepend('<li>Requested page ' + page + '</li>');
 		}
 	});
