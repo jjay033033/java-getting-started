7c89f2925f0c961bc0a05c91bc62534bbecfa450
LMoon <jjay033033@gmail.com>
2017-11-07 10:38:57 +0800

    123

--- src/main/java/top/lmoon/shadowsupdate/config/XmlConfig.java
+++ src/main/java/top/lmoon/shadowsupdate/config/XmlConfig.java
@@ -46,7 +46,7 @@ public class XmlConfig {
 	
 	private void initServerMap(){
 		try {
-			this.serverMap = null;
+			this.serverMap.clear();
 			Map servers = (Map) map.get("servers");
 			if (servers == null || servers.isEmpty()) {
 				throw new Exception("没有servers项！");
