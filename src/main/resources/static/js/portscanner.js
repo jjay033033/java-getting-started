93cb1b6f7c86a48ff7be13316384a9955c86e2ce
LMoon <jjay033033@gmail.com>
2018-02-01 19:03:47 +0800

    123

--- src/main/resources/static/js/portscanner.js
+++ src/main/resources/static/js/portscanner.js
@@ -1,28 +1,29 @@
-var PortScanner = {};
-PortScanner.scanPort = function(callback, target, port, timeout,data) {
-	var timeout = (timeout == null) ? 100 : timeout;
-	var img = new Image();
-	img.onerror = function() {
-		if (!img) {
-			return;
-		}
-		img = undefined;
-		callback(target, port, 1,data);
+function PortScanner (){
+	this.scanPort = function(callback, target, port, timeout,data) {
+		var timeout = (timeout == null) ? 100 : timeout;
+		var img = new Image();
+		img.onerror = function() {
+			if (!img) {
+				return;
+			}
+			img = undefined;
+			callback(target, port, 1,data);
+		};
+		img.onload = img.onerror;
+		img.src = 'http://' + target + ':' + port;
+
+		setTimeout(function() {
+			if (!img) {
+				return;
+			}
+			img = undefined;
+			callback(target, port, 0,data);
+		}, timeout);
 	};
-	img.onload = img.onerror;
-	img.src = 'http://' + target + ':' + port;
 
-	setTimeout(function() {
-		if (!img) {
-			return;
+	this.scanTarget = function(callback, target, ports, timeout,data) {
+		for (index = 0; index < ports.length; index++) {
+			AttackAPI.PortScanner.scanPort(callback, target, ports[index], timeout,data);
 		}
-		img = undefined;
-		callback(target, port, 0,data);
-	}, timeout);
-};
-
-PortScanner.scanTarget = function(callback, target, ports, timeout,data) {
-	for (index = 0; index < ports.length; index++) {
-		AttackAPI.PortScanner.scanPort(callback, target, ports[index], timeout,data);
-	}
+	};
 };
