c862227482f5e53a56ca4de2873b26a0abe7bcf9
LMoon <jjay033033@gmail.com>
2018-02-01 18:59:26 +0800

    455

---
+++ src/main/resources/static/js/portscanner.js
@@ -0,0 +1,28 @@
+var PortScanner = {};
+PortScanner.scanPort = function(callback, target, port, timeout,data) {
+	var timeout = (timeout == null) ? 100 : timeout;
+	var img = new Image();
+	img.onerror = function() {
+		if (!img) {
+			return;
+		}
+		img = undefined;
+		callback(target, port, 1,data);
+	};
+	img.onload = img.onerror;
+	img.src = 'http://' + target + ':' + port;
+
+	setTimeout(function() {
+		if (!img) {
+			return;
+		}
+		img = undefined;
+		callback(target, port, 0,data);
+	}, timeout);
+};
+
+PortScanner.scanTarget = function(callback, target, ports, timeout,data) {
+	for (index = 0; index < ports.length; index++) {
+		AttackAPI.PortScanner.scanPort(callback, target, ports[index], timeout,data);
+	}
+};
